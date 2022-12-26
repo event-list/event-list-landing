a1787a0ac4b82f744643174d34bc45130fd13765
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-12-26 02:37:12 -0300

    fix: hydration

--- src/components/ui/container/Container.tsx
+++ src/components/ui/container/Container.tsx
@@ -1,10 +1,7 @@
-import {
-  Box,
-  Container as ChakraContainer,
-  Stack,
-} from '@chakra-ui/react';
+import { Box, Container as ChakraContainer, Stack } from '@chakra-ui/react';
+import React from 'react';
 
-const Container = (props) => {
+const Container = (props: { children: React.ReactNode }) => {
   return (
     <ChakraContainer maxW={'100vw'} h={'100vh'}>
       <Stack
@@ -14,7 +11,9 @@ const Container = (props) => {
         h={'100vh'}
         zIndex={1}
       >
-        <Stack h={'50vh'} justifyContent={'flex-end'} zIndex={1}>{props.children}</Stack>
+        <Stack h={'50vh'} justifyContent={'flex-end'} zIndex={1}>
+          {props.children}
+        </Stack>
       </Stack>
     </ChakraContainer>
   );
