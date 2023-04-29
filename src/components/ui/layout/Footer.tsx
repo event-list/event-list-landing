3e2635c90c7a54db082cbb1d72edababed358a95
Vinicius <viblaziusgoulart@gmail.com>
2023-04-29 18:21:08 -0300

    feat(footer): footer

--- src/components/ui/layout/Footer.tsx
+++ src/components/ui/layout/Footer.tsx
@@ -4,11 +4,13 @@ import {
   Button,
   Stack,
   Text,
-  VisuallyHidden,
   useColorModeValue,
+  VisuallyHidden,
+  Link,
+  SimpleGrid,
 } from '@chakra-ui/react';
-import { ReactNode } from 'react';
-import { FaGithub, FaInstagram } from 'react-icons/fa';
+import type { ReactNode } from 'react';
+import { FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
 import { Logo } from '../Logo';
 
 const SocialButton = ({
@@ -45,44 +47,60 @@ const SocialButton = ({
   );
 };
 
+const ListHeader = ({ children }: { children: ReactNode }) => {
+  return (
+    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
 export default function Footer() {
   return (
     <Box
-      borderTop={'1px'}
-      borderColor='gray.800'
       role="contentinfo"
-      position="absolute"
+      borderColor='gray.800'
+      position="relative"
       bottom={'0'}
       left={'0'}
       right={'0'}
       as={'footer'}
       bg={'gray.900'}
       color={'gray.200'}
+      borderTop="1px"
+      py="10"
+      borderTopColor={useColorModeValue('gray.200', 'gray.700')}
     >
-      <Container
-        as={Stack}
-        maxW={'6xl'}
-        py={4}
-        direction={{ base: 'column', md: 'row' }}
-        spacing={4}
-        justify={{ base: 'center', md: 'space-between' }}
-        align={{ base: 'center', md: 'center' }}
-      >
-        <Box h="10">
-          <Logo />
-        </Box>
-        <Text>© 2022 Event List. All rights reserved</Text>
-        <Stack direction={'row'} spacing={6}>
-          <SocialButton label={'GitHub'} href={'https://github.com/event-list'}>
-            <FaGithub />
-          </SocialButton>
-          <SocialButton
-            label={'Instagram'}
-            href={'https://www.instagram.com/eventlist.ev/'}
-          >
-            <FaInstagram />
-          </SocialButton>
-        </Stack>
+      <Container as={Stack} maxW={'6xl'}>
+        <SimpleGrid templateColumns={{ sm: '1fr 1fr', md: '2fr 2fr 2fr' }} spacing={8}>
+          <Stack align={'flex-start'}>
+            <ListHeader>Company</ListHeader>
+            <Link href={'/'}>About us</Link>
+            <Link href={'mailto:viblaziusgoulart@gmail.com'}>Contact us</Link>
+            <Link href={'services#prices'}>Pricing</Link>
+          </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Support</ListHeader>
+            <Link href={'#'}>Terms of Service</Link>
+          </Stack>
+          <Stack spacing={6}>
+            <Box w="40">
+              <Logo />
+            </Box>
+            <Text fontSize={'sm'}>© {new Date().getFullYear()} Event List. All rights reserved</Text>
+            <Stack direction={'row'} spacing={6}>
+              <SocialButton label={'Twitter'} href={'#'}>
+                <FaTwitter />
+              </SocialButton>
+              <SocialButton label={'Github'} href={'https://github.com/event-list'}>
+                <FaGithub />
+              </SocialButton>
+              <SocialButton label={'Instagram'} href={'https://www.instagram.com/evtlist/'}>
+                <FaInstagram />
+              </SocialButton>
+            </Stack>
+          </Stack>
+        </SimpleGrid>
       </Container>
     </Box>
   );
